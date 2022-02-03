bf7168087311c8ca779533e3dde88367937b24cb
ndmsts <98880772+ndmsts@users.noreply.github.com>
2022-02-03 20:14:08 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,16 @@
+public abstract class TangibleAsset {
+  private String name;
+  private int price;
+  private String color;
+  
+  public TangibleAsset(String name, int price, Sting color) {
+    this.name = name;
+    this,price = price;
+    this.color = color;
+  }
+  
+  public String getName() {retrun this.name; }
+  public int getPrice() {retrun this.price; }
+  public String getColor() {retrun this.color; }
+  
+}
